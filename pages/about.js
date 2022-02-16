dfbd7567aac1d142ae701020374cde88fb69e163
Gulbike Biyekenova <gbknva@gmail.com>
2022-02-16 14:30:07 -0500

    add about page

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Meta from "../components/Meta"
+
+
+export default function About() {
+  return (
+    <div>
+      <Meta title='About'/>
+      <h1>Welcome to About Page</h1>
+    </div>
+  )
+}
